7237e11b7f81638db8c093f7258dac29b29a9311
Ilyuhsquette <ilyushquette@gmail.com>
2017-04-25 20:51:32 +0300

    #3: Debugger core entity. Particle tracking behavior

---
+++ scape2d/scape2d-debugger/src/main/scala/scape/scape2d/debugger/Debugger.scala
@@ -0,0 +1,11 @@
+package scape.scape2d.debugger
+
+import scape.scape2d.debugger.view.DebugView
+import scape.scape2d.engine.matter.Particle
+
+class Debugger(val view:DebugView) {
+  def trackParticle(particle:Particle) = {
+    view.renderParticle(particle);
+    particle.addMotionListener(view.renderMotion(_));
+  }
+}
