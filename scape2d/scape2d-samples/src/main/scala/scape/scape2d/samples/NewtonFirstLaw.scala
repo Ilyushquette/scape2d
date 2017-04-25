619a08d2e6b4a9f070b5c05910fe17bbba9aa80f
Ilyuhsquette <ilyushquette@gmail.com>
2017-04-25 20:54:28 +0300

    #3: Provided visualization of Newton's first law simulation

--- scape2d/scape2d-samples/src/main/scala/scape/scape2d/samples/NewtonFirstLaw.scala
+++ scape2d/scape2d-samples/src/main/scala/scape/scape2d/samples/NewtonFirstLaw.scala
@@ -1,9 +1,15 @@
 package scape.scape2d.samples
 
+import java.awt.Color
+import javax.swing.JFrame
 import scape.scape2d.engine.core.Nature
 import scape.scape2d.engine.geom.Point2D
 import scape.scape2d.engine.geom.Vector2D
 import scape.scape2d.engine.matter.Particle
+import scape.scape2d.debugger.view.swing.SwingShapeDrawer
+import scape.scape2d.debugger.Debugger
+import scape.scape2d.debugger.view.ShapeDrawingDebugView
+import java.awt.Toolkit
 
 object NewtonFirstLaw {
   def main(args:Array[String]):Unit = {
@@ -11,5 +17,15 @@ object NewtonFirstLaw {
     val metalParticle = new Particle(Point2D.origin, 5, 2, new Vector2D(2, 45));
     nature.addParticle(metalParticle);
     nature.start;
+    
+    val frame = new JFrame("Scape2D Debugger");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBackground(Color.BLACK);
+    val shapeDrawer = new SwingShapeDrawer(Toolkit.getDefaultToolkit().getScreenSize(), Color.BLACK);
+    val debugger = new Debugger(new ShapeDrawingDebugView(shapeDrawer, 2));
+    frame.add(shapeDrawer);
+    frame.pack();
+    frame.setVisible(true);
+    debugger.trackParticle(metalParticle);
   }
 }
