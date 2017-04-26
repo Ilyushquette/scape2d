0a975403a8a60b338392dc7ef63afc0b79f29aa8
Ilyushquette <ilyushquette@gmail.com>
2017-04-26 23:52:34 +0300

    #4: Unit tests for Vector2D sum and resolution from components

--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/geom/GeomFunctionsTest.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/geom/GeomFunctionsTest.scala
@@ -1,12 +1,30 @@
 package scape.scape2d.engine.geom
 
-import org.junit.Test
 import org.junit.Assert
+import org.junit.Test
 
 class TestGeomFunctions {
   @Test
-  def testCalculateComponents() = {
+  def testCalculateComponents = {
     val vector = new Vector2D(7, 15);
     Assert.assertEquals(Components2D(6.761480784023478, 1.8117333157176452), calculateComponents(vector));
+  }
+  
+  @Test
+  def testCalculateVector = {
+    val components = Components2D(6.761480784023478, 1.8117333157176452);
+    Assert.assertEquals(new Vector2D(7, 14.999999999999998), calculateVector(components));
+  }
+  
+  @Test
+  def testSumOpposedVectors = {
+    val vector = sum(new Vector2D(20, 0), new Vector2D(30, 180));
+    Assert.assertEquals(new Vector2D(10, 179.99999999999997), vector);
+  }
+  
+  @Test
+  def testSumUnidirectionalVectors = {
+    val vector = sum(new Vector2D(10, 45), new Vector2D(7, 45), new Vector2D(30, 45));
+    Assert.assertEquals(new Vector2D(47, 45), vector);
   }
 }
