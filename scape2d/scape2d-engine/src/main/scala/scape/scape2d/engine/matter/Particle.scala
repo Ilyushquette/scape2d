eeac5fbcfa741692e31b6a5360ee9d1574181496
Ilyushquette <ilyushquette@gmail.com>
2017-05-08 21:10:18 +0300

    #5: Integration of timestep impulses with velocity through acceleration resolution using Newton's second law of motion

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/matter/Particle.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/matter/Particle.scala
@@ -1,9 +1,10 @@
 package scape.scape2d.engine.matter
 
 import scala.collection.mutable.LinkedHashSet
-
 import scape.scape2d.engine.geom.Point2D
 import scape.scape2d.engine.geom.Vector2D
+import org.apache.log4j.Logger
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Units:
@@ -16,8 +17,25 @@ import scape.scape2d.engine.geom.Vector2D
  */
 class Particle(val position:Point2D, val radius:Double, val mass:Double, var velocity:Vector2D) {
   private[engine] val motionListeners = new LinkedHashSet[MotionEvent => Unit];
+  private val log = Logger.getLogger(getClass);
+  private[engine] val forces = new ArrayBuffer[Vector2D];
   
   def addMotionListener(listener:MotionEvent => Unit) = motionListeners.add(listener);
   
   def removeMotionListener(listener:MotionEvent => Unit) = motionListeners.remove(listener);
+  
+  /**
+   * Since each force here is a representation of impulse J = N x timestep,
+   * acceleration in meters per second per timestep too.
+   * Final velocity of the particle in meters per second.
+   */
+  def integrateForces(timestep:Long) = {
+    if(!forces.isEmpty) {
+      val netforce = forces.reduce(_ + _);
+      val acceleration = new Vector2D(netforce.magnitude / mass, netforce.angle);
+      log.debug("Netforce %s sum of %s caused acceleration %s".format(netforce, forces, acceleration));
+      velocity = velocity + acceleration;
+      forces.clear();
+    }
+  }
 }
