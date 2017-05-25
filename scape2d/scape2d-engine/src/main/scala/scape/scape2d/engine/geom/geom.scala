2df62957044a1117dca1766126042fde9d646453
Ilyushquette <ilyushquette@gmail.com>
2017-05-25 21:45:44 +0300

    #8: Shortcut for sin / cos evaluation from degrees

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/geom.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/geom.scala
@@ -1,5 +1,11 @@
 package scape.scape2d.engine
 
+import java.lang.Math._
+
 package object geom {
   def normalizeAngle(angle:Double) = (angle + 360) % 360;
+  
+  def sinDeg(angle:Double) = sin(toRadians(normalizeAngle(angle)));
+  
+  def cosDeg(angle:Double) = cos(toRadians(normalizeAngle(angle)));
 }
