731b1388fb352765a786668ef6816de82ece63f4
Ilyushquette <ilyushquette@gmail.com>
2017-06-11 16:58:16 +0300

    #10: Two hundred particles simulation

---
+++ scape2d/scape2d-samples/src/main/scala/scape/scape2d/samples/TwoHundredParticles.scala
@@ -0,0 +1,50 @@
+package scape.scape2d.samples
+
+import java.awt.Color
+import java.awt.Toolkit
+
+import javax.swing.JFrame
+import scape.scape2d.debugger.Debugger
+import scape.scape2d.debugger.view.ShapeDrawingDebugView
+import scape.scape2d.debugger.view.swing.SwingShapeDrawer
+import scape.scape2d.engine.core.Nature
+import scape.scape2d.engine.core.matter.ParticleBuilder
+import scape.scape2d.engine.geom.Point2D
+import scape.scape2d.engine.geom.Vector2D
+import scape.scape2d.engine.motion.MovableTrackerProxy
+
+object TwoHundredParticles {
+  def main(args:Array[String]):Unit = {
+    val nature = new Nature(60);
+    val metalParticles = for(i <- 0 to 100) yield ParticleBuilder()
+      .at(new Point2D(i * 0.11, i * 0.11))
+      .withRadius(5)
+      .withMass(2)
+      .withVelocity(new Vector2D(2, 0))
+      .build;
+    val metalParticles2 = for(i <- 0 to 100) yield ParticleBuilder()
+      .at(new Point2D(25 - i * 0.11, i * 0.11))
+      .withRadius(5)
+      .withMass(2)
+      .withVelocity(new Vector2D(2, 180))
+      .build;
+    
+    val trackedMetalParticles = metalParticles.map(new MovableTrackerProxy(_));
+    val trackedMetalParticles2 = metalParticles2.map(new MovableTrackerProxy(_));
+    
+    val frame = new JFrame("Scape2D Debugger");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBackground(Color.BLACK);
+    val shapeDrawer = new SwingShapeDrawer(Toolkit.getDefaultToolkit().getScreenSize(), Color.BLACK);
+    val debugger = new Debugger(new ShapeDrawingDebugView(shapeDrawer, 2));
+    frame.add(shapeDrawer);
+    frame.pack();
+    frame.setVisible(true);
+    
+    trackedMetalParticles.foreach(debugger.trackParticle(_));
+    trackedMetalParticles2.foreach(debugger.trackParticle(_));
+    trackedMetalParticles.foreach(nature.add(_));
+    trackedMetalParticles2.foreach(nature.add(_));
+    nature.start;
+  }
+}
