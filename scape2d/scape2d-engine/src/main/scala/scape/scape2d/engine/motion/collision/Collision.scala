9a84b2d544e315c3a75e5464d296a28d7307d7f8
Ilyushquette <ilyushquette@gmail.com>
2017-06-28 17:19:42 +0300

    #11: Abstract detector of collisions between movables during specified timestep

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/collision/Collision.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/collision/Collision.scala
@@ -1,8 +1,7 @@
 package scape.scape2d.engine.motion.collision
 
 import scape.scape2d.engine.core.Movable
-import scape.scape2d.engine.geom.Formed
 
-case class Collision[T <: Movable[T] with Formed[_]](concurrentPair:(T, T), time:Double) {
+case class Collision[T <: Movable[T]](concurrentPair:(T, T), time:Double) {
   val snapshotPair = (concurrentPair._1.snapshot, concurrentPair._2.snapshot);
 }
