ef7db471da90ecbbbc5959bc60e97b70ddbd98ca
Ilyushquette <ilyushquette@gmail.com>
2017-08-26 22:33:04 +0400

    #14: Bond structure tracker proxy now tracks down bond fracture and introduces StructureBreakEvent

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/deformation/BondStructureTrackerProxy.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/deformation/BondStructureTrackerProxy.scala
@@ -26,6 +26,7 @@ object BondStructureTrackerProxy {
 
 class BondStructureTrackerProxy(val delegate:Bond) extends MethodInterceptor {
   private var structureEvolutionListeners = new HashSet[StructureEvolutionEvent => Unit]();
+  private var structureBreakListeners = new HashSet[StructureBreakEvent => Unit]();
   lazy val enhanced = BondStructureTrackerProxy.enhance(this);
   
   def addStructureEvolutionListener(listener:StructureEvolutionEvent => Unit) = {
@@ -36,6 +37,14 @@ class BondStructureTrackerProxy(val delegate:Bond) extends MethodInterceptor {
     structureEvolutionListeners.remove(listener);
   }
   
+  def addStructureBreakListener(listener:StructureBreakEvent => Unit) = {
+    structureBreakListeners.add(listener);
+  }
+  
+  def removeStructureBreakListener(listener:StructureBreakEvent => Unit) = {
+    structureBreakListeners.remove(listener);
+  }
+  
   def intercept(obj:Object, method:Method, args:Array[Object], methodProxy:MethodProxy) = {
     val methodName = method.getName();
     if("setDeformationDescriptor" == methodName) args match {
@@ -43,6 +52,8 @@ class BondStructureTrackerProxy(val delegate:Bond) extends MethodInterceptor {
         handleStructureEvolution(obj.asInstanceOf[Bond], methodProxy, evolved);
       case _ =>
         methodProxy.invokeSuper(obj, args);
+    }else if("break" == methodName) {
+      handleStructureBreak(obj.asInstanceOf[Bond], methodProxy, args);
     }else if("reversed" == methodName) args match {
       case Array() =>
         reverseAndEnhanceBond(obj.asInstanceOf[Bond], methodProxy);
@@ -59,10 +70,17 @@ class BondStructureTrackerProxy(val delegate:Bond) extends MethodInterceptor {
     result;
   }
   
+  private def handleStructureBreak(proxy:Bond, methodProxy:MethodProxy, args:Array[Object]) = {
+    val result = methodProxy.invokeSuper(proxy, args);
+    structureBreakListeners.foreach(_(StructureBreakEvent(proxy)));
+    result;
+  }
+  
   private def reverseAndEnhanceBond(proxy:Bond, methodProxy:MethodProxy) = {
     val reversedBond = methodProxy.invokeSuper(proxy, Array.empty).asInstanceOf[Bond];
     val structureTrackedReversedBond = new BondStructureTrackerProxy(reversedBond);
     structureTrackedReversedBond.structureEvolutionListeners = structureEvolutionListeners;
+    structureTrackedReversedBond.structureBreakListeners = structureBreakListeners;
     structureTrackedReversedBond.enhanced;
   }
 }
---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/deformation/StructureBreakEvent.scala
@@ -0,0 +1,5 @@
+package scape.scape2d.engine.deformation
+
+import scape.scape2d.engine.core.matter.Bond
+
+case class StructureBreakEvent(broken:Bond);
