d01cff72d7898872079f9647b5b2d0fb4fcf2b52
Ilyushquette <ilyushquette@gmail.com>
2017-09-02 14:29:08 +0300

    #15: Raster abstract rectangular grid of pixels or points of color (RGB)

---
+++ scape2d/scape2d-graphics/src/main/scala/scape/scape2d/graphics/raster/Raster.scala
@@ -0,0 +1,16 @@
+package scape.scape2d.graphics.raster
+
+import scape.scape2d.engine.geom.shape.AxisAlignedRectangleInteger
+import scape.scape2d.engine.geom.shape.PointInteger
+
+trait Raster {
+  def bounds:AxisAlignedRectangleInteger;
+  
+  def getRgb(point:PointInteger):Int = getRgb(point.x, point.y);
+  
+  def getRgb(x:Int, y:Int):Int;
+  
+  def setRgb(point:PointInteger, rgb:Int):Unit = setRgb(point.x, point.y, rgb);
+  
+  def setRgb(x:Int, y:Int, rgb:Int):Unit;
+}
