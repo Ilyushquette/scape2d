fd698e6eda55a7342c914f43a9a2ca602436c14c
Ilyushquette <ilyushquette@gmail.com>
2017-11-05 15:29:25 +0300

    #16: Body entity with center particle (center of mass), composition of bonds of particles and angular velocity pseudovector

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/core/matter/Body.scala
@@ -0,0 +1,25 @@
+package scape.scape2d.engine.core.matter
+
+import scape.scape2d.engine.core.Rotatable
+
+class Body(
+  val centerParticle:Particle,
+  val bonds:Set[Bond],
+  private var _angularVelocity:Double
+) extends Rotatable {
+  val movables = bonds.foldLeft(Set[Particle]())((particles, bond) => particles + bond.particles._1 + bond.particles._2);
+  
+  def center = centerParticle.position;
+  
+  def angularVelocity = _angularVelocity;
+  
+  private[core] def setAngularVelocity(newAngularVelocity:Double) = _angularVelocity = newAngularVelocity;
+  
+  def snapshot = snapshot();
+  
+  def snapshot(centerParticle:Particle = this.centerParticle,
+               bonds:Set[Bond] = this.bonds,
+               angularVelocity:Double = this.angularVelocity) = {
+    new Body(centerParticle, bonds, angularVelocity);
+  }
+}
