ee7dc1afb67c1de0d10990812deac2aca8c94ffe
Ilyushquette <ilyushquette@gmail.com>
2017-11-11 13:19:10 +0300

    #16: Post-rotation position prediction

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/rotational/rotational.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/rotational/rotational.scala
@@ -1,6 +1,22 @@
 package scape.scape2d.engine.motion
 
+import java.lang.Math.toDegrees
+
+import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.geom.normalizeAngle
+import scape.scape2d.engine.geom.Vector2D
+
 package object rotational {
+  def getPostRotationPosition(movable:Movable, timestep:Double) = {
+    if(movable.rotatable.isDefined && movable.rotatable.get.angularVelocity != 0) {
+      val radiansPerTimestep = asRadiansPerTimestep(movable.rotatable.get.angularVelocity, timestep);
+      val degreesPerTimestep = normalizeAngle(toDegrees(radiansPerTimestep));
+      val radialVector = movable.position - movable.rotatable.get.center;
+      val nextRadialVector = new Vector2D(radialVector.magnitude, radialVector.angle + degreesPerTimestep);
+      movable.rotatable.get.center.displace(nextRadialVector.components);
+    }else movable.position;
+  }
+  
   def asRadiansPerTimestep(angularVelocity:Double, timestep:Double) = {
     val stepsPerSecond = 1000 / timestep;
     angularVelocity / stepsPerSecond;
--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/motion/rotational/RotationalFunctionsTest.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/motion/rotational/RotationalFunctionsTest.scala
@@ -2,6 +2,10 @@ package scape.scape2d.engine.motion.rotational
 
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.Mockito
+import scape.scape2d.engine.geom.shape.Point
+import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.core.Rotatable
 
 class RotationalFunctionsTest {
   @Test
@@ -26,5 +30,56 @@ class RotationalFunctionsTest {
   def testAsRadiansPerSecondClockwise = {
     val radiansPerTimestep = -3.14159; // 180 degrees
     Assert.assertEquals(-31.4159, asRadiansPerSecond(radiansPerTimestep, 100), 0.00001);
+  }
+  
+  @Test
+  def testPostRotationPositionWithoutRotatable = {
+    val movableMock = Mockito.mock(classOf[Movable]);
+    Mockito.when(movableMock.position).thenReturn(Point(4, 4));
+    Mockito.when(movableMock.rotatable).thenReturn(None);
+    Assert.assertEquals(Point(4, 4), getPostRotationPosition(movableMock, 1000));
+  }
+  
+  @Test
+  def testPostRotationPositionZeroAngularVelocity = {
+    val rotatableMock = Mockito.mock(classOf[Rotatable]);
+    Mockito.when(rotatableMock.center).thenReturn(Point.origin);
+    Mockito.when(rotatableMock.angularVelocity).thenReturn(0);
+    
+    val movableMock = Mockito.mock(classOf[Movable]);
+    Mockito.when(movableMock.position).thenReturn(Point(0, 1));
+    Mockito.when(movableMock.rotatable).thenReturn(Some(rotatableMock));
+    
+    Assert.assertEquals(Point(0, 1), getPostRotationPosition(movableMock, 1000));
+  }
+  
+  @Test
+  def testPostRotationPositionCounterclockwise = {
+    val rotatableMock = Mockito.mock(classOf[Rotatable]);
+    Mockito.when(rotatableMock.center).thenReturn(Point.origin);
+    Mockito.when(rotatableMock.angularVelocity).thenReturn(1.5708); // 90 degrees
+    
+    val movableMock = Mockito.mock(classOf[Movable]);
+    Mockito.when(movableMock.position).thenReturn(Point(0, 1));
+    Mockito.when(movableMock.rotatable).thenReturn(Some(rotatableMock));
+    
+    val postRotationPosition = getPostRotationPosition(movableMock, 1000);
+    Assert.assertEquals(-1, postRotationPosition.x, 0.00001);
+    Assert.assertEquals(0, postRotationPosition.y, 0.00001);
+  }
+  
+  @Test
+  def testPostRotationPositionClockwise = {
+    val rotatableMock = Mockito.mock(classOf[Rotatable]);
+    Mockito.when(rotatableMock.center).thenReturn(Point.origin);
+    Mockito.when(rotatableMock.angularVelocity).thenReturn(-0.785398); // -45 degrees
+    
+    val movableMock = Mockito.mock(classOf[Movable]);
+    Mockito.when(movableMock.position).thenReturn(Point(1, 0));
+    Mockito.when(movableMock.rotatable).thenReturn(Some(rotatableMock));
+    
+    val postRotationPosition = getPostRotationPosition(movableMock, 1000);
+    Assert.assertEquals(0.70710, postRotationPosition.x, 0.00001);
+    Assert.assertEquals(-0.70710, postRotationPosition.y, 0.00001);
   }
 }
