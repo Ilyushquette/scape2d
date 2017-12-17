9e4c5caac9c617bee3e33a185d3180ad7ed98f28
Ilyushquette <ilyushquette@gmail.com>
2017-12-17 14:29:54 +0300

    Frequencies equal if duration between occurences equal

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/Frequency.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/Frequency.scala
@@ -2,4 +2,9 @@ package scape.scape2d.engine.time
 
 case class Frequency(occurences:Double, duration:Duration) {
   lazy val occurenceDuration = Duration(duration.value / occurences, duration.unit);
+  
+  override def equals(any:Any) = any match {
+    case other:Frequency => occurenceDuration == other.occurenceDuration;
+    case _ => false;
+  }
 }
--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/time/FrequencyTest.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/time/FrequencyTest.scala
@@ -9,4 +9,24 @@ class FrequencyTest {
     val frequency = Frequency(5, Second);
     Assert.assertEquals(0.2(Second), frequency.occurenceDuration);
   }
+  
+  @Test
+  def testDifferentFrequenciesWithSameTypes = {
+    Assert.assertFalse(Frequency(3, Millisecond) == Frequency(2, Millisecond));
+  }
+  
+  @Test
+  def testSameFrequenciesWithSameTypes = {
+    Assert.assertTrue(Frequency(5, Second) == Frequency(15, 3(Second)));
+  }
+  
+  @Test
+  def testDifferentFrequenciesWithDifferentTypes = {
+    Assert.assertFalse(Frequency(1, Millisecond) == Frequency(999, Second));
+  }
+  
+  @Test
+  def testSameFrequenciesWithDifferentTypes = {
+    Assert.assertTrue(Frequency(5, Second) == Frequency(300, Minute));
+  }
 }
