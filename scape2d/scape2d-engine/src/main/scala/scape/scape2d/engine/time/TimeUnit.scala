7571c2ca8421eb7baefbe380ed3aead51c186dc4
Ilyushquette <ilyushquette@gmail.com>
2017-12-17 10:48:33 +0300

    Added time units ranging from Nanosecond to Day

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/TimeUnit.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/TimeUnit.scala
@@ -3,3 +3,27 @@ package scape.scape2d.engine.time
 sealed trait TimeUnit {
   def milliseconds:Double;
 }
+
+case object Day extends TimeUnit {
+  val milliseconds = Hour.milliseconds * 24;
+}
+
+case object Hour extends TimeUnit {
+  val milliseconds = Minute.milliseconds * 60;
+}
+
+case object Minute extends TimeUnit {
+  val milliseconds = Second.milliseconds * 60;
+}
+
+case object Second extends TimeUnit {
+  val milliseconds = Millisecond.milliseconds * 1000;
+}
+
+case object Millisecond extends TimeUnit {
+  val milliseconds = 1d;
+}
+
+case object Nanosecond extends TimeUnit {
+  val milliseconds = Millisecond.milliseconds / 1000000;
+}
