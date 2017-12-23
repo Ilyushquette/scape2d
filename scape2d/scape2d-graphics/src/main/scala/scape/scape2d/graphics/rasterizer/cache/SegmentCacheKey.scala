365251d703abf9798c2515293445c166428983b9
Ilyushquette <ilyushquette@gmail.com>
2017-12-23 20:47:47 +0300

    Reduced matching precision of slope of the segment cache key. Monadic resolution of truncated slope

--- scape2d/scape2d-graphics/src/main/scala/scape/scape2d/graphics/rasterizer/cache/SegmentCacheKey.scala
+++ scape2d/scape2d-graphics/src/main/scala/scape/scape2d/graphics/rasterizer/cache/SegmentCacheKey.scala
@@ -4,12 +4,7 @@ import scape.scape2d.engine.geom.shape.Segment
 
 private[cache] class SegmentCacheKey(segment:Segment) {
   val truncatedLength = Math.round(segment.length * 10000000000d) / 10000000000d;
-  val truncatedSlope = {
-    if(segment.line.slope.isDefined)
-      Some(Math.round(segment.line.slope.get * 10000000000d) / 10000000000d);
-    else
-      None;
-  }
+  val truncatedSlope = segment.line.slope.map(slope => Math.round(slope * 1000d) / 1000d);
   
   override def hashCode = truncatedSlope.hashCode + truncatedLength.hashCode;
     
