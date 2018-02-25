1aaa866a662bf2873a549f86e8b8cb2168a6ade9
Ilyushquette <ilyushquette@gmail.com>
2018-02-25 19:38:25 +0300

    #20: ExpandedTree. Expands in eight directions by specified amount using provided node factory (by default uses simple buckets)

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/partition/ExpandedTree.scala
@@ -0,0 +1,39 @@
+package scape.scape2d.engine.geom.partition
+
+import scape.scape2d.engine.geom.Formed
+import scape.scape2d.engine.geom.shape.AxisAlignedRectangle
+import scape.scape2d.engine.geom.shape.CompositeShape
+import scape.scape2d.engine.geom.shape.Point
+import scape.scape2d.engine.geom.shape.ShapeBounds
+import scape.scape2d.engine.geom.shape.Shape
+
+class ExpandedTree[E <: Formed[_ <: Shape]](
+  val coreNode:Node[E],
+  val nodeFactory:AxisAlignedRectangle => Node[E] = new Bucket[E](_),
+  val expansion:Double
+) extends Node[E] {
+  val topLeft = AxisAlignedRectangle(Point(coreNode.bounds.topLeft.x - expansion, coreNode.bounds.topLeft.y), expansion, expansion);
+  val topCenter = AxisAlignedRectangle(coreNode.bounds.topLeft, coreNode.bounds.width, expansion);
+  val topRight = AxisAlignedRectangle(coreNode.bounds.topRight, expansion, expansion);
+  val centerLeft = AxisAlignedRectangle(Point(topLeft.topLeft.x, coreNode.bounds.bottomLeft.y), expansion, coreNode.bounds.height);
+  val centerRight = AxisAlignedRectangle(coreNode.bounds.bottomRight, expansion, coreNode.bounds.height);
+  val bottomLeft = AxisAlignedRectangle(Point(topLeft.topLeft.x, coreNode.bounds.bottomLeft.y - expansion), expansion, expansion);
+  val bottomCenter = AxisAlignedRectangle(bottomLeft.bottomRight, coreNode.bounds.width, expansion);
+  val bottomRight = AxisAlignedRectangle(bottomCenter.bottomRight, expansion, expansion);
+  
+  val regions = List(topLeft, topCenter, topRight, centerLeft, centerRight, bottomLeft, bottomCenter, bottomRight);
+  val bounds = ShapeBounds(CompositeShape(regions));
+  val nodes = coreNode +: regions.map(nodeFactory);
+  
+  private var _entities:List[E] = List.empty;
+  
+  def entities = _entities;
+  
+  def insert(entity:E) = {
+    if(bounds.contains(entity.shape)){
+      val inserted = nodes.exists(_ insert entity);
+      if(!inserted) _entities = _entities :+ entity;
+      true;
+    }else false;
+  }
+}
