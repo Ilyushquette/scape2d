e3ee5fa95bde5184260d1e5a789bd7d3232743c6
Ilyushquette <ilyushquette@gmail.com>
2018-02-27 22:49:53 +0300

    #20: Tree based collision detector. Creates tree by provided factory and enriches it with movables to detect collisions between using provided collision detection strategy

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/collision/detection/TreeCollisionDetector.scala
@@ -0,0 +1,56 @@
+package scape.scape2d.engine.motion.collision.detection
+
+import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.geom.shape.Shape
+import scape.scape2d.engine.geom.Formed
+import scape.scape2d.engine.geom.partition.Node
+import scape.scape2d.engine.motion.MotionBounds
+import scape.scape2d.engine.motion.collision.CollisionEvent
+import scala.collection.mutable.HashSet
+
+case class TreeCollisionDetector[T <: Movable with Formed[_ <: Shape]](
+  treeFactory:() => Node[MotionBounds[T]], 
+  detectionStrategy:CollisionDetectionStrategy[T]
+) extends CollisionDetector[T] {
+  private val treeCreationListeners = HashSet[Node[MotionBounds[T]] => Unit]();
+  
+  def detect(movables:Iterable[T], timestep:Double) = {
+    val tree = treeFactory();
+    movables.foreach(m => tree.insert(MotionBounds(m, timestep)));
+    treeCreationListeners.foreach(_(tree));
+    val collisions = detectNodeCollisions(tree, timestep);
+    collisions.iterator;
+  }
+  
+  private def detectNodeCollisions(tree:Node[MotionBounds[T]], timestep:Double):Iterable[CollisionEvent[T]] = {
+    if(!tree.entities.isEmpty) {
+      val entityDetections = detectEntityCollisions(tree, timestep);
+      val subEntityDetections = detectSubEntityCollisions(tree, timestep);
+      val detections = entityDetections ++ subEntityDetections;
+      val nodeCollisions = detections.flatten;
+      nodeCollisions ++ tree.nodes.flatMap(detectNodeCollisions(_, timestep));
+    }else tree.nodes.flatMap(detectNodeCollisions(_, timestep));
+  }
+  
+  private def detectEntityCollisions(tree:Node[MotionBounds[T]], timestep:Double) = {
+    for {
+      motionBounds1 <- tree.entities
+      motionBounds2 <- tree.entities
+      if(motionBounds1.movable != motionBounds2.movable)
+    } yield detect(motionBounds1.movable, motionBounds2.movable, timestep);
+  }
+  
+  private def detectSubEntityCollisions(tree:Node[MotionBounds[T]], timestep:Double) = {
+    for(motionBounds <- tree.entities; subMotionBounds <- tree.nodes.flatMap(_.subEntities))
+    yield detect(motionBounds.movable, subMotionBounds.movable, timestep);
+  }
+  
+  private def detect(movable1:T, movable2:T, timestep:Double) = {
+    val detection = detectionStrategy.detect(movable1, movable2, timestep);
+    detection.map(time => CollisionEvent((movable1, movable2), time));
+  }
+  
+  def onTreeCreation(listener:Node[MotionBounds[T]] => Unit) = treeCreationListeners += listener;
+  
+  def offTreeCreation(listener:Node[MotionBounds[T]] => Unit) = treeCreationListeners -= listener;
+}
