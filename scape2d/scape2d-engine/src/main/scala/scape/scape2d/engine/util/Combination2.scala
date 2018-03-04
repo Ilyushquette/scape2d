46474a225063baba9ee52042e0f13d25a4d8868f
Ilyushquette <ilyushquette@gmail.com>
2018-03-04 11:38:56 +0300

    #20: Selection of combinations of 2 from generic collection

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/util/Combination2.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/util/Combination2.scala
@@ -1,5 +1,17 @@
 package scape.scape2d.engine.util
 
+object Combination2 {
+  def selectFrom[T](iterable:Iterable[T]) = makeCombinations(iterable.toList, Set[Combination2[T, T]]());
+  
+  private def makeCombinations[T](list:List[T], combinations:Set[Combination2[T, T]]):Set[Combination2[T, T]] = {
+    list match {
+      case Nil => combinations;
+      case List(x) => combinations;
+      case x::xs => makeCombinations(xs, combinations ++ xs.map(Combination2(x, _)));
+    }
+  }
+}
+
 case class Combination2[T1, T2](_1:T1, _2:T2) {
   lazy val reversed = Combination2(_2, _1);
   
--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/util/Combination2Test.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/util/Combination2Test.scala
@@ -15,4 +15,28 @@ class Combination2Test {
   
   @Test
   def testReversed = Assert.assertEquals(Combination2(321, 123), Combination2(123, 321).reversed);
+  
+  @Test
+  def testSelectFromEmptyCollectionEmptyCombinations = {
+    val numbers = Set[Int]();
+    Assert.assertEquals(Set(), Combination2.selectFrom(numbers));
+  }
+  
+  @Test
+  def testSelectFromSingletonCollectionEmptyCombinations = {
+    val numbers = Set(3);
+    Assert.assertEquals(Set(), Combination2.selectFrom(numbers));
+  }
+  
+  @Test
+  def testSelectFromCollectionDistinctCombinations = {
+    val numbers = Set(1, 3, 7, 4);
+    val expectedCombinations = Set(Combination2(1, 3),
+                                   Combination2(1, 7),
+                                   Combination2(1, 4),
+                                   Combination2(3, 7),
+                                   Combination2(3, 4),
+                                   Combination2(7, 4));
+    Assert.assertEquals(expectedCombinations, Combination2.selectFrom(numbers));
+  }
 }
