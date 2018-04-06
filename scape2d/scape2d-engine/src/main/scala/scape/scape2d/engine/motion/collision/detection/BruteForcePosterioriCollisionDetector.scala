4c65fa464dea805802961bc14189c3652c1c9450
Ilyushquette <ilyushquette@gmail.com>
2018-04-06 23:14:56 +0300

    #21: Brute-force based posteriori collision detector

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/collision/detection/BruteForcePosterioriCollisionDetector.scala
@@ -0,0 +1,17 @@
+package scape.scape2d.engine.motion.collision.detection
+
+import scala.collection.immutable.Set
+import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.geom.shape.Shape
+import scape.scape2d.engine.geom.Formed
+import scape.scape2d.engine.util.Combination2
+import scape.scape2d.engine.motion.collision.CollisionEvent
+
+case class BruteForcePosterioriCollisionDetector[T <: Movable with Formed[_ <: Shape]]
+extends PosterioriCollisionDetector[T] {
+  def detect(movables:Set[T]) = {
+    val combinations = Combination2.selectFrom(movables);
+    val overlapping = combinations.filter(combination => combination._1.shape intersects combination._2.shape);
+    overlapping.map(CollisionEvent(_, 0));
+  }
+}
