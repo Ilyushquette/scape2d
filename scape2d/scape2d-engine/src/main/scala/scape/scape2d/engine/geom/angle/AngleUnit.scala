cad754bc362629fda2b7d352dcefd18d08643d79
Ilyushquette <ilyushquette@gmail.com>
2018-05-05 12:18:41 +0300

    Radian and Degree angle units of measurement

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/AngleUnit.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/AngleUnit.scala
@@ -1,7 +1,19 @@
 package scape.scape2d.engine.geom.angle
 
+import java.lang.Math.PI
+
 sealed trait AngleUnit {
   def radians:Double;
   
   def upperBound:Double;
+}
+
+case object Degree extends AngleUnit {
+  val radians = PI / 180;
+  val upperBound = 360d;
+}
+
+case object Radian extends AngleUnit {
+  val radians = 1d;
+  val upperBound = PI * 2;
 }
