412697f295b31d21553d5e8be17a1793ed2138fa
Ilyushquette <ilyushquette@gmail.com>
2018-05-05 12:38:34 +0300

    Epsilon radians value

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/package.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/package.scala
@@ -3,5 +3,7 @@ package scape.scape2d.engine.geom
 import scape.scape2d.engine.geom.angle.DoubleAngle
 
 package object angle {
+  val Epsilon = 1E-10;
+  
   implicit def doubleToAngle(value:Double) = DoubleAngle(value);
 }
