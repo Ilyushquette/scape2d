98140bfd6ec10a42101b07bc7d5e11e3ea2742e8
Ilyushquette <ilyushquette@gmail.com>
2018-05-05 12:32:00 +0300

    Implicit conversion of angle unit to angle with value equal to 1

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/AngleUnit.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/AngleUnit.scala
@@ -2,6 +2,10 @@ package scape.scape2d.engine.geom.angle
 
 import java.lang.Math.PI
 
+object AngleUnit {
+  implicit def toAngle(unit:AngleUnit) = Angle.bound(1, unit);
+}
+
 sealed trait AngleUnit {
   def radians:Double;
   
