8e64fb9024abd6dae91d12078a29b6d4c094e07b
Ilyushquette <ilyushquette@gmail.com>
2018-05-22 17:59:14 +0300

    Cosine and sine of Angle

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/package.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/package.scala
@@ -6,4 +6,8 @@ package object angle {
   val Epsilon = 1E-10;
   
   implicit def doubleToAngle(value:Double) = DoubleAngle(value);
+  
+  def cos(angle:Angle) = java.lang.Math.cos(angle.radians);
+  
+  def sin(angle:Angle) = java.lang.Math.sin(angle.radians);
 }
