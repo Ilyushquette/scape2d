b604dced3d85972a88f33c7529867e88f75da089
Ilyushquette <ilyushquette@gmail.com>
2018-05-25 22:20:41 +0300

    Current moment of time instant creation

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/Instant.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/time/Instant.scala
@@ -1,3 +1,10 @@
 package scape.scape2d.engine.time
 
+object Instant {
+  def now() = {
+    val unixEpochDuration = Duration(System.currentTimeMillis(), Millisecond);
+    Instant(unixEpochDuration);
+  }
+}
+
 case class Instant(unixEpochDuration:Duration);
