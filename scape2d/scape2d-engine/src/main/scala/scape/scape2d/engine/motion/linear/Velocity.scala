d4eb197fdc73e57aa80e34681a12c03b8c618bb6
Ilyushquette <ilyushquette@gmail.com>
2018-05-26 17:46:59 +0300

    Comparison of velocities by displacement vectors over the same time

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/Velocity.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/Velocity.scala
@@ -6,8 +6,10 @@ import scape.scape2d.engine.geom.Vector
 /**
  * distance in meters
  */
-case class Velocity(vector:Vector, time:Duration) {
+case class Velocity(vector:Vector, time:Duration) extends Ordered[Velocity] {
   lazy val vectorPerMillisecond = vector / time.milliseconds;
+  
+  def compare(velocity:Velocity) = vectorPerMillisecond compareTo velocity.vectorPerMillisecond;
   
   override def equals(any:Any) = any match {
     case velocity:Velocity => vectorPerMillisecond == velocity.vectorPerMillisecond;
