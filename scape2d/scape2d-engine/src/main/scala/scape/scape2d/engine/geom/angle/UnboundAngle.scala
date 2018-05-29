541893ba651afd8768d0d8bc058b50b193b781bb
Ilyushquette <ilyushquette@gmail.com>
2018-05-29 19:56:20 +0300

    Comparison of unbound angles

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/UnboundAngle.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/angle/UnboundAngle.scala
@@ -1,9 +1,12 @@
 package scape.scape2d.engine.geom.angle
 
+import com.google.common.math.DoubleMath.fuzzyCompare
 import com.google.common.math.DoubleMath.fuzzyEquals
 
-case class UnboundAngle(value:Double, unit:AngleUnit) {
+case class UnboundAngle(value:Double, unit:AngleUnit) extends Ordered[UnboundAngle] {
   lazy val radians = value * unit.radians;
+  
+  def compare(unboundAngle:UnboundAngle) = fuzzyCompare(radians, unboundAngle.radians, Epsilon);
   
   override def equals(any:Any) = any match {
     case unboundAngle:UnboundAngle => fuzzyEquals(radians, unboundAngle.radians, Epsilon);
