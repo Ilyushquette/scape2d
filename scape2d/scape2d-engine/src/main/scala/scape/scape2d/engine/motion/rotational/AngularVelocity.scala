f0b9624b791271a1abae5c61185eb146938eb63f
Ilyushquette <ilyushquette@gmail.com>
2018-05-30 12:05:01 +0300

    Comparison of angular velocities by displacement angles over the same time

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/rotational/AngularVelocity.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/rotational/AngularVelocity.scala
@@ -4,14 +4,19 @@ import scape.scape2d.engine.geom.angle.UnboundAngle
 import scape.scape2d.engine.time.Duration
 import scape.scape2d.engine.geom.angle.Angle
 import scape.scape2d.engine.geom.angle.Epsilon
+import com.google.common.math.DoubleMath.fuzzyCompare
 import com.google.common.math.DoubleMath.fuzzyEquals
 
 object AngularVelocity {
   def apply(angle:Angle, time:Duration) = new AngularVelocity(angle.unbound, time);
 }
 
-case class AngularVelocity(angle:UnboundAngle, time:Duration) {
+case class AngularVelocity(angle:UnboundAngle, time:Duration) extends Ordered[AngularVelocity] {
   lazy val radiansPerMillisecond = angle.radians / time.milliseconds;
+  
+  def compare(angularVelocity:AngularVelocity) = {
+    fuzzyCompare(radiansPerMillisecond, angularVelocity.radiansPerMillisecond, Epsilon);
+  }
   
   override def equals(any:Any) = any match {
     case angularVelocity:AngularVelocity =>
