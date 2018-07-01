c4972264eb32819156e90bd6f49a96c948624320
Ilyushquette <ilyushquette@gmail.com>
2018-07-01 23:37:37 +0300

    Ton, Kilogram, Gram and Milligram mass units of measurement

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/MassUnit.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/MassUnit.scala
@@ -3,3 +3,19 @@ package scape.scape2d.engine.mass
 sealed trait MassUnit {
   def kilograms:Double;
 }
+
+case object Ton extends MassUnit {
+  val kilograms = 1000d;
+}
+
+case object Kilogram extends MassUnit {
+  val kilograms = 1d;
+}
+
+case object Gram extends MassUnit {
+  val kilograms = 0.001;
+}
+
+case object Milligram extends MassUnit {
+  val kilograms = Gram.kilograms / 1000;
+}
