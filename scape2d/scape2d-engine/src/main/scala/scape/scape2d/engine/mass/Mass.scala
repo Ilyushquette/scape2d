8db007e7d4fce5071f604a0a77bbc8cff92f0126
Ilyushquette <ilyushquette@gmail.com>
2018-07-01 23:39:50 +0300

    Comparison of masses

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/Mass.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/Mass.scala
@@ -1,9 +1,12 @@
 package scape.scape2d.engine.mass
 
+import com.google.common.math.DoubleMath.fuzzyCompare
 import com.google.common.math.DoubleMath.fuzzyEquals
 
-case class Mass(value:Double, unit:MassUnit) {
+case class Mass(value:Double, unit:MassUnit) extends Ordered[Mass] {
   lazy val kilograms = value * unit.kilograms;
+  
+  def compare(mass:Mass) = fuzzyCompare(kilograms, mass.kilograms, Epsilon);
   
   override def equals(any:Any) = any match {
     case mass:Mass => fuzzyEquals(kilograms, mass.kilograms, Epsilon);
