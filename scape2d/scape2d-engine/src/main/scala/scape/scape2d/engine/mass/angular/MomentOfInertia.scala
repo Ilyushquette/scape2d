4366577a5532060cd47304c48582fb1f61d302d3
Ilyushquette <ilyushquette@gmail.com>
2018-07-03 14:07:19 +0300

    Instant angular acceleration resolution for torque from moment of inertia

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/angular/MomentOfInertia.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/mass/angular/MomentOfInertia.scala
@@ -3,7 +3,12 @@ package scape.scape2d.engine.mass.angular
 import com.google.common.math.DoubleMath.fuzzyCompare
 import com.google.common.math.DoubleMath.fuzzyEquals
 
+import scape.scape2d.engine.geom.angle.Radian
+import scape.scape2d.engine.geom.angle.UnboundAngle
 import scape.scape2d.engine.mass.Mass
+import scape.scape2d.engine.motion.rotational.InstantAngularAcceleration
+import scape.scape2d.engine.time.Second
+import scape.scape2d.engine.time.TimeUnit.toDuration
 
 object MomentOfInertia {
   def apply(mass:Mass, distanceToCenter:Double) = {
@@ -14,6 +19,11 @@ object MomentOfInertia {
 case class MomentOfInertia private[MomentOfInertia](
   value:Double
 ) extends Ordered[MomentOfInertia] {
+  def forTorque(torque:Double) = {
+    val magnitude = torque / value;
+    InstantAngularAcceleration(UnboundAngle(magnitude, Radian) / Second);
+  }
+  
   def compare(momentOfInertia:MomentOfInertia) = fuzzyCompare(value, momentOfInertia.value, Epsilon);
   
   override def equals(any:Any) = any match {
--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/mass/angular/MomentOfInertiaTest.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/mass/angular/MomentOfInertiaTest.scala
@@ -3,8 +3,12 @@ package scape.scape2d.engine.mass.angular
 import org.junit.Test
 import org.junit.Assert
 
+import scape.scape2d.engine.geom.angle.UnboundAngle
+import scape.scape2d.engine.geom.angle.Radian
 import scape.scape2d.engine.mass.doubleToMass
 import scape.scape2d.engine.mass.Kilogram
+import scape.scape2d.engine.motion.rotational.InstantAngularAcceleration
+import scape.scape2d.engine.time.Second
 
 class MomentOfInertiaTest {
   @Test
@@ -15,5 +19,13 @@ class MomentOfInertiaTest {
   @Test
   def testMomentsOfInertiaEqual = {
     Assert.assertTrue(MomentOfInertia(25(Kilogram), 2) == MomentOfInertia(4(Kilogram), 5));
+  }
+  
+  @Test
+  def testInstantAngularAccelerationResolutionForTorqueFromMomentOfInertia = {
+    val momentOfInertia = MomentOfInertia(5(Kilogram), 4);
+    val torque = 40;
+    val expectedAngularAcceleration = InstantAngularAcceleration(UnboundAngle(-0.5, Radian) / Second);
+    Assert.assertEquals(expectedAngularAcceleration, momentOfInertia forTorque -40);
   }
 }
