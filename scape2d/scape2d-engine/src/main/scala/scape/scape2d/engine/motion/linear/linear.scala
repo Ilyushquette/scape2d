f0e16dda14b80900854beb1123999110c0ce4a7a
Ilyushquette <ilyushquette@gmail.com>
2018-08-25 23:13:23 +0300

    #23: Linear displacement function of time

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/linear.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/linear.scala
@@ -1,20 +1,22 @@
 package scape.scape2d.engine.motion
 
 import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.geom.Vector
 import scape.scape2d.engine.geom.Vector.toComponents
 import scape.scape2d.engine.geom.shape.Point
 import scape.scape2d.engine.time.Duration
 import scape.scape2d.engine.geom.shape.Shape
 
 package object linear {
+  def displacementForTimeOf(movable:Movable[_ <: Shape]):(Duration => Vector) = {
+    val v = movable.velocity;
+    if(movable.isMovingLinearly) v forTime _;
+    else _ => Vector.zero;
+  }
+  
   def positionForTimeOf(movable:Movable[_ <: Shape]):(Duration => Point) = {
-    val position = movable.position;
-    val velocity = movable.velocity;
-    if(movable.isMovingLinearly) {
-      timestep => {
-        val displacementVector = velocity.forTime(timestep);
-        position + displacementVector;
-      }
-    }else _ => position;
+    val p = movable.position;
+    val vt = displacementForTimeOf(movable);
+    t => p displacedBy vt(t);
   }
 }
--- scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/motion/linear/LinearMotionFunctionsTest.scala
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/motion/linear/LinearMotionFunctionsTest.scala
@@ -2,18 +2,32 @@ package scape.scape2d.engine.motion.linear
 
 import org.junit.Assert
 import org.junit.Test
-import scape.scape2d.engine.core.Movable
-import scape.scape2d.engine.geom.angle.Degree
-import scape.scape2d.engine.geom.angle.doubleToAngle
+
+import scape.scape2d.engine.core.MovableMock
 import scape.scape2d.engine.geom.Components
 import scape.scape2d.engine.geom.Vector
+import scape.scape2d.engine.geom.angle.AngleUnit.toAngle
+import scape.scape2d.engine.geom.angle.Degree
+import scape.scape2d.engine.geom.angle.doubleToAngle
 import scape.scape2d.engine.geom.shape.Point
 import scape.scape2d.engine.time.Duration
 import scape.scape2d.engine.time.Millisecond
 import scape.scape2d.engine.time.Second
-import scape.scape2d.engine.core.MovableMock
+import scape.scape2d.engine.time.TimeUnit.toDuration
 
 class LinearMotionFunctionsTest {
+  @Test
+  def testLinearDisplacementZero = {
+    val movableMock = new MovableMock(Point.origin, Velocity.zero, None);
+    Assert.assertEquals(Vector.zero, displacementForTimeOf(movableMock)(Duration(1, Second)));
+  }
+  
+  @Test
+  def testLinearDisplacement = {
+    val movableMock = new MovableMock(Point(3, 5), Vector.from(Components(0, 6)) / Second, None);
+    Assert.assertEquals(Vector(3, 90(Degree)), displacementForTimeOf(movableMock)(Duration(500, Millisecond)));
+  }
+  
   @Test
   def testPostLinearMotionPositionZeroDisplacement = {
     val movableMock = new MovableMock(Point(3, 5), Velocity.zero, None);
