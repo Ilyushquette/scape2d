70faa6b88dd4180e5bf83e71d63e9fdcb93d388d
Ilyushquette <ilyushquette@gmail.com>
2018-08-25 23:15:42 +0300

    #23: Reimplemented position function of time for movables using rotated position and linear displacement functions of time

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/motion.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/motion.scala
@@ -1,23 +1,20 @@
 package scape.scape2d.engine
 
 import scape.scape2d.engine.core.Movable
+import scape.scape2d.engine.geom.Vector.toComponents
 import scape.scape2d.engine.geom.shape.Point
-import scape.scape2d.engine.motion.linear.{positionForTimeOf => postLinearMotionPosition}
-import scape.scape2d.engine.motion.rotational.{positionForTimeOf => postRotationPosition}
+import scape.scape2d.engine.motion.linear.displacementForTimeOf
+import scape.scape2d.engine.motion.rotational.{ positionForTimeOf => rotatedPositionForTimeOf }
 import scape.scape2d.engine.time.Duration
 import scape.scape2d.engine.geom.shape.Shape
 
 package object motion {
   def positionForTimeOf(movable:Movable[_ <: Shape]):(Duration => Point) = {
     val position = movable.position;
-    val ft_linear = postLinearMotionPosition(movable);
-    val ft_rotational = postRotationPosition(movable);
-    t => if(t > Duration.zero) {
-      val p1 = ft_linear(t);
-      val p2 = ft_rotational(t);
-      val angularDisplacementVector = p2 - position;
-      p1 + angularDisplacementVector;
-    }else position;
+    val rotatedPositionForTime = rotatedPositionForTimeOf(movable);
+    val linearDisplacementForTime = displacementForTimeOf(movable);
+    t => if(t > Duration.zero) rotatedPositionForTime(t) + linearDisplacementForTime(t);
+         else position;
   }
   
   def distanceForTimeOf(movable1:Movable[_ <: Shape], movable2:Movable[_ <: Shape]):(Duration => Double) = {
