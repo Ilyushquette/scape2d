8a7c6bbaab64d702baec0f21acf2063a62cf4d37
Ilyushquette <ilyushquette@gmail.com>
2018-09-03 18:39:45 +0300

    #24: Density - mass per area (square meters)

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/density/Density.scala
@@ -0,0 +1,19 @@
+package scape.scape2d.engine.density
+
+import com.google.common.math.DoubleMath.fuzzyCompare
+import com.google.common.math.DoubleMath.fuzzyEquals
+
+import scape.scape2d.engine.geom.AreaEpsilon
+import scape.scape2d.engine.mass.Mass
+
+case class Density(mass:Mass, area:Double) extends Ordered[Density] {
+  lazy val kilogramsPerSquareMeter = mass.kilograms / area;
+  
+  override def equals(any:Any) = any match {
+    case density:Density =>
+      fuzzyEquals(kilogramsPerSquareMeter, density.kilogramsPerSquareMeter, AreaEpsilon);
+    case _ => false;
+  }
+  
+  def compare(density:Density) = fuzzyCompare(kilogramsPerSquareMeter, density.kilogramsPerSquareMeter, AreaEpsilon);
+}
