115f85d471921c17ec08d841e3fadf53c2727efe
Ilyushquette <ilyushquette@gmail.com>
2018-09-04 21:32:11 +0300

    #24: Second moment of area epsilon = distance epsilon to the fourth power

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/geom.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/geom.scala
@@ -11,6 +11,7 @@ import scape.scape2d.engine.geom.shape.Segment
 package object geom {
   val Epsilon = 1E-10;
   val AreaEpsilon = Epsilon * Epsilon;
+  val SecondMomentOfAreaEpsilon = Epsilon * Epsilon * Epsilon * Epsilon;
   val TwicePI = PI * 2;
   
   def findMutualX(l1:Line, l2:Line) = {
