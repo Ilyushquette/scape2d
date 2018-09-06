53e4910e7e3a1ed47dab75e76bdb7388a52bd646
Ilyushquette <ilyushquette@gmail.com>
2018-09-06 20:14:26 +0300

    #25: Impulse onto rigid body pluggable as time dependent into rigid body dynamics

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/core/matter/ImpulseOntoRigidBody.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/core/matter/ImpulseOntoRigidBody.scala
@@ -1,8 +1,9 @@
 package scape.scape2d.engine.core.matter
 
-import scape.scape2d.engine.geom.Vector
 import scape.scape2d.engine.core.TimeDependent
+import scape.scape2d.engine.geom.Vector
 import scape.scape2d.engine.time.Duration
+import scape.scape2d.engine.geom.shape.FiniteShape
 
 /**
  * Units:
@@ -10,13 +11,13 @@ import scape.scape2d.engine.time.Duration
  *  <li>force - magnitude in Newtons</li>
  * </ul>
  */
-case class Impulse(
-  particle:Particle,
+case class ImpulseOntoRigidBody(
+  rigidBody:RigidBody[_ >: Null <: FiniteShape],
   force:Vector,
-  time:Duration)
-extends TimeDependent {
+  time:Duration
+) extends TimeDependent {
   private[core] def integrate(timestep:Duration) = {
     val multiplier = timestep / time;
-    particle.exertForce(force * multiplier, true);
+    rigidBody.exertForce(force * multiplier, rigidBody.position);
   }
 }
