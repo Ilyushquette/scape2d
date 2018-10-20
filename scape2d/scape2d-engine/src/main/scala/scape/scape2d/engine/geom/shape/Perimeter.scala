22508d22394b4358b35139259281d6374050d0c2
Ilyushquette <ilyushquette@gmail.com>
2018-10-20 18:15:18 +0300

    #26: Abstract finite perimeter with length available

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/shape/Perimeter.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/geom/shape/Perimeter.scala
@@ -3,3 +3,9 @@ package scape.scape2d.engine.geom.shape
 sealed trait Perimeter {
   def shape:Shape;
 }
+
+sealed trait FinitePerimeter extends Perimeter {
+  def shape:FiniteShape;
+  
+  def length:Double;
+}
