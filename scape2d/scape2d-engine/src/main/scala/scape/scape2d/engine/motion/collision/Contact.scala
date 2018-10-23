b1672ab15640a6c5812253e92b6d4e1dc5b11a53
Ilyushquette <ilyushquette@gmail.com>
2018-10-23 23:54:46 +0300

    #27: Contact entity containing information about point and angle of contact (depends on the perspective)

---
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/collision/Contact.scala
@@ -0,0 +1,6 @@
+package scape.scape2d.engine.motion.collision
+
+import scape.scape2d.engine.geom.angle.Angle
+import scape.scape2d.engine.geom.shape.Point
+
+case class Contact(point:Point, angle:Angle);
