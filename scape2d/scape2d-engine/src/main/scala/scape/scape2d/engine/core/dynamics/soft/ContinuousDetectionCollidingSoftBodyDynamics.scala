c7cad74d3c8ba1c3084f173cafa14a4372436e69
Ilyushquette <ilyushquette@gmail.com>
2018-11-05 19:29:13 +0300

    Dropped collision detector and its implementations in favor of continuous broad phase collision detection strategy

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/core/dynamics/soft/ContinuousDetectionCollidingSoftBodyDynamics.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/core/dynamics/soft/ContinuousDetectionCollidingSoftBodyDynamics.scala
@@ -2,22 +2,27 @@ package scape.scape2d.engine.core.dynamics.soft
 
 import scape.scape2d.engine.core.dynamics.Dynamics
 import scape.scape2d.engine.core.matter.Particle
-import scape.scape2d.engine.motion.collision.detection.BruteForceCollisionDetector
-import scape.scape2d.engine.motion.collision.detection.CollisionDetector
+import scape.scape2d.engine.motion.collision.CollisionEvent
+import scape.scape2d.engine.motion.collision.detection.CollisionDetectionStrategy
 import scape.scape2d.engine.motion.collision.detection.IterativeRootFindingCollisionDetectionStrategy
+import scape.scape2d.engine.motion.collision.detection.broad.BruteForceContinuousBroadPhaseCollisionDetectionStrategy
+import scape.scape2d.engine.motion.collision.detection.broad.ContinuousBroadPhaseCollisionDetectionStrategy
 import scape.scape2d.engine.time.Duration
+import scape.scape2d.engine.util.Combination2
 
 case class ContinuousDetectionCollidingSoftBodyDynamics(
-  collisionDetector:CollisionDetector[Particle] = BruteForceCollisionDetector(
-      detectionStrategy = IterativeRootFindingCollisionDetectionStrategy()
-  ),
+  broadPhaseDetectionStrategy:ContinuousBroadPhaseCollisionDetectionStrategy[Particle] =
+    BruteForceContinuousBroadPhaseCollisionDetectionStrategy(),
+  detectionStrategy:CollisionDetectionStrategy[Particle] =
+    IterativeRootFindingCollisionDetectionStrategy(),
   collisionResolver:ParticleCollisionResolver = ParticleCollisionResolver(),
   softBodyDynamics:SoftBodyDynamics = new SoftBodyDynamics()
 ) extends Dynamics {
   def movables = softBodyDynamics.movables;
   
   def integrate(timestep:Duration) = {
-    val collisions = collisionDetector.detect(softBodyDynamics.movables, timestep);
+    val suspectedCollidingParticles = broadPhaseDetectionStrategy.prune(movables, timestep);
+    val collisions = suspectedCollidingParticles.flatMap(detectCollision(_, timestep));
     if(!collisions.isEmpty) {
       val earliestCollision = collisions.minBy(_.time);
       if(earliestCollision.time > Duration.zero)
@@ -27,5 +32,10 @@ case class ContinuousDetectionCollidingSoftBodyDynamics(
       if(remainingTime > Duration.zero)
         integrate(remainingTime);
     }else softBodyDynamics.integrate(timestep);
+  }
+  
+  private def detectCollision(particles:Combination2[Particle, Particle], timestep:Duration) = {
+    val detection = detectionStrategy.detect(particles._1, particles._2, timestep);
+    detection.map(CollisionEvent(particles, _));
   }
 }
