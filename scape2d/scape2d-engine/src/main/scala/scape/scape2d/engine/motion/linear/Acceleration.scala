1e50fc29cdee4b6c4641ea659ebecd190eb0d8ae
Ilyushquette <ilyushquette@gmail.com>
2018-11-14 21:43:52 +0300

    Time for scalar velocity change resolution from acceleration

--- scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/Acceleration.scala
+++ scape2d/scape2d-engine/src/main/scala/scape/scape2d/engine/motion/linear/Acceleration.scala
@@ -2,4 +2,8 @@ package scape.scape2d.engine.motion.linear
 
 import scape.scape2d.engine.time.Duration
 
-case class Acceleration(velocity:Velocity, time:Duration);
+case class Acceleration(velocity:Velocity, time:Duration) {
+  def forScalarVelocityChange(magnitude:Double, time:Duration) = {
+    this.time * (magnitude / velocity.forTime(time).magnitude);
+  }
+}
---
+++ scape2d/scape2d-engine/src/test/scala/scape/scape2d/engine/motion/linear/AccelerationTest.scala
@@ -0,0 +1,19 @@
+package scape.scape2d.engine.motion.linear
+
+import org.junit.Assert
+import org.junit.Test
+
+import scape.scape2d.engine.geom.Vector
+import scape.scape2d.engine.geom.angle.Angle
+import scape.scape2d.engine.time.doubleToTime
+import scape.scape2d.engine.time.Millisecond
+import scape.scape2d.engine.time.Minute
+import scape.scape2d.engine.time.Second
+
+class AccelerationTest {
+  @Test
+  def testTimeForScalarVelocityChangeResolution = {
+    val acceleration = Vector(5, Angle.right) / Second / Second;
+    Assert.assertEquals(500(Millisecond), acceleration.forScalarVelocityChange(150, Minute));
+  }
+}
